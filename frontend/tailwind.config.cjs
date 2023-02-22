bbfaf59674bd68711602db9dae8838c3041082f6
Carolina leon <leoncarolina35@gmail.com>
2023-02-22 17:37:30 +0100

    caro modifs

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,13 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [ "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+    "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
       extend: {
         colors: {
           dark: "hsl(0deg 0% 8%)",
           netflixRed: "hsl(357deg 92% 47%)",
           subMain:'#9A48FF',
+          main: '#0E101B',
+          dry:'#0B0F29',
+          star: '#FFB000',
+          text: '#C0C0C0',
+          border: '#4b5563',
+          dryGray: '#E0D5D5',
+        },
+        height:{
+          header: '560px',
+          rate:'400px',
+        },
+        fontSize: {
+          h1:'2.6rem',
+        },
+        screens:{
+          xs: '475px',
         },
         animation: {
           "slide-rtl": "slide-rtl .4s ease-in-out",
@@ -20,6 +36,5 @@ module.exports = {
         },
       },
     },
-    plugins: [],
+    plugins: [require('@tailwindcss/line-clamp')],
   };
-  
